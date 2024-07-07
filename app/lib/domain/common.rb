90611b1d8e66a3463f051efebaddf4927c3bb12b
noracami <kerke.2011@gmail.com>
2024-07-08 03:21:00 +0800

    🐛 do not check the empty grid is_blocked

--- app/lib/domain/common.rb
+++ app/lib/domain/common.rb
@@ -24,13 +24,37 @@ module Domain
       end
 
       def grid_and_its_neighbors_on_the_map(grid, pastures)
-        connect_grids(grid).select { |g| pastures.include?(g) } + [grid]
+        grid = grid.with_indifferent_access
+        (
+          [grid] +
+              [
+                { x: grid[:x] + 1, y: grid[:y] },
+                { x: grid[:x] - 1, y: grid[:y] },
+                { x: grid[:x], y: grid[:y] + 1 },
+                { x: grid[:x], y: grid[:y] - 1 },
+                { x: grid[:x] + 1, y: grid[:y] - 1 },
+                { x: grid[:x] - 1, y: grid[:y] + 1 }
+              ]
+            .map do |g|
+              pastures.select do |pasture|
+                pasture['x'] == g[:x] && pasture['y'] == g[:y]
+              end.first
+            end.compact
+        ).map(&:with_indifferent_access)
       end
 
       def all_neighbors_capture?(grid, pastures)
-        connect_grids(grid)
-          .select { |g| pastures.include?(g) && g['stack']['amount'].positive? }
-          .all? { |g| g['stack']['amount'].positive? }
+        grid = grid.with_indifferent_access
+        (
+          [
+            { x: grid[:x] + 1, y: grid[:y] },
+            { x: grid[:x] - 1, y: grid[:y] },
+            { x: grid[:x], y: grid[:y] + 1 },
+            { x: grid[:x], y: grid[:y] - 1 },
+            { x: grid[:x] + 1, y: grid[:y] - 1 },
+            { x: grid[:x] - 1, y: grid[:y] + 1 }
+          ].map { |g| (pastures.select { |pasture| pasture['x'] == g[:x] && pasture['y'] == g[:y] }).first }
+        ).compact.map(&:with_indifferent_access).all? { |g| g['stack']['amount'].positive? }
       end
     end
   end

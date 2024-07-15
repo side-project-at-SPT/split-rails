8c0c87b202e3f29e4658bbf920f0cc637e726075
noracami <kerke.2011@gmail.com>
2024-07-16 01:51:28 +0800

    🩹 try require net/http

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class UsersController < BaseController
+      require 'net/http'
+
       skip_before_action :load_jwt_request, only: %i[create login_via_gaas_token]
 
       # GET /api/v1/users

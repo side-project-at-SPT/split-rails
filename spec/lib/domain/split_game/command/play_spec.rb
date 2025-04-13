dd5b2d4e1850639afb3220be71192102603ab231
noracami <kerke.2011@gmail.com>
2025-04-13 16:05:28 +0800

    🤡 mock response (bot#generate_grid_to_place_stack)

--- spec/lib/domain/split_game/command/play_spec.rb
+++ spec/lib/domain/split_game/command/play_spec.rb
@@ -4,18 +4,28 @@ RSpec.describe Domain::SplitGame::Command::Play do
   let(:room) { Room.create(name: 'room') }
   let(:user) { Visitor.new_visitor }
   let(:bot) { Bot.create(owner: user, name: 'bot', webhook_url: 'https://localhost:3000/mybot/c8763') }
-  let(:player) { Visitor.find(bot.join_room(room)) }
+  let(:bot_player) { Visitor.find(bot.join_room(room)) }
   let(:another_player) { Visitor.new_visitor(role: :test_dummy) }
   let(:game) do
-    player.touch
+    bot_player.touch
     room.players << another_player
     room.save
-    room.start_new_game
+    room.start_new_game(seed: 0)
+  end
+
+  before do
+    mock_http = double('HTTPX')
+    mock_response = double('Response')
+
+    allow(HTTPX).to receive(:with).and_return(mock_http)
+    allow(mock_http).to receive(:with).and_return(mock_http)
+    allow(mock_http).to receive(:get).and_return(mock_response)
+    allow(mock_response).to receive(:json).and_return({ 'ip' => '8.8.8.8', 'city' => 'Mountain View' })
   end
 
   it 'places a stack' do
     expect do
-      described_class.new(game:, player:).call
+      described_class.new(game:, player: bot_player).call
     end.not_to raise_error
 
     expect(game.steps.last.step_type).to eq('place_stack')
